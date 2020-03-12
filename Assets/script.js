e12bdf0396d80d46ada4c9cf5afc976172d78b8c
Tucker Hembree <bladeofxp@gmail.com>
2020-03-11 22:03:32 -0400

    finally got my removal feature to work, but am essentially out of time; copied + pasted rough code from first workaround to create next question pages; genuinely, this was a terrible way to do it, but I just don't have enough time to deal with it; next is to get the sub form, final score display, and high score list pages working

--- Assets/script.js
+++ Assets/script.js
@@ -20,7 +20,6 @@ var quizFooter = document.getElementById("quiz-footer");
 var quizTitleText = document.getElementById("quiz-title");
 var quizStartText = document.getElementById("quiz-start-text");
 var quizStartButton = document.getElementById("start-button");
-
 var timeLeft = 0;
 
 timerEl.textContent = ": " + timeLeft;
@@ -34,22 +33,12 @@ function countdownTimer() {
     timerEl.textContent = ": " + timeLeft;
     timeLeft--;
 
-    // if (questionResponse === false) {
-    //   timeLeft -= 10;
-    // }
-    // | finalResponseClick === true
-    if (timeLeft < 0) {
+    if (timeLeft < 0 | quizTitleText.textContent === "And that's it!") {
       clearInterval(timeInterval);
       highscoreBar.setAttribute("class", "collapse");
       finalScore = timeLeft;
     }
   }, 1000);
-}
-
-function quizStartSelect() {
-  quizTitleText.innerHTML = questions[0].question;
-  quizStartText.setAttribute("class", "collapse");
-  quizStartButton.setAttribute("class", "collapse"); 
 }
 
 var questionOneAnswers = [
@@ -84,51 +73,211 @@ var questionFiveAnswers = [
 ];
 
 var questions = [
-    {
-      question: "1) Commonly used data types DO NOT include: ",
-      answer: "c"
-    },
-    {
-      question:
-        "2) The condition in an if/else statement is enclosed within ____.",
-      answer: "c"
-    },
-    {
-      question: "3) Arrays in Javascript can be used to store ____. ",
-      answer: "d"
-    },
-    {
-      question:
-        "4) String values must be enclosed within ____ when being assigned variables.",
-      answer: "c"
-    },
-    {
-      question:
-        "5) A very useful tool used during development and debugging for printing content to the debugger is ____.",
-      answer: "d"
-    }
-  ]; 
-
-function createFirstButtons() {
-  console.log(questionOneAnswers.length);
-  for (i = 0; i < questionOneAnswers.length; i++) {
-    var lineBreaker = document.createElement("div");
-    lineBreaker.setAttribute("class", "row m-1");
-    quizBody.appendChild(lineBreaker);
-    var firstButtons = document.createElement("button");
-    firstButtons.setAttribute(
-      "class",
-      "btn btn-primary btn-sm button-left-margin"
-    );
-    firstButtons.setAttribute("data-answer", questionOneAnswers[i]);
-    firstButtons.innerHTML = questionOneAnswers[i];
-    lineBreaker.appendChild(firstButtons);
+  {
+    question: 
+      "1) Commonly used data types DO NOT include: "
+  },
+  {
+    question:
+      "2) The condition in an if/else statement is enclosed within ____."
+  },
+  {
+    question: 
+      "3) Arrays in Javascript can be used to store ____."
+  },
+  {
+    question:
+      "4) String values must be enclosed within ____ when being assigned variables."
+  },
+  {
+    question:
+      "5) A very useful tool used during development and debugging for printing content to the debugger is ____."
   }
-}
+];
 
 quizStartButton.addEventListener("click", function() {
   countdownTimer();
   quizStartSelect();
   createFirstButtons();
-
 });
+
+function quizStartSelect() {
+  quizTitleText.innerHTML = questions[0].question;
+  quizStartText.setAttribute("class", "collapse");
+  quizStartButton.setAttribute("class", "collapse");
+}
+
+function createFirstButtons() {
+  var divCollector = document.createElement("div");
+  divCollector.setAttribute("class", "delete-me");
+  quizBody.appendChild(divCollector);
+  for (i = 0; i < questionOneAnswers.length; i++) {
+    var lineBreaker = document.createElement("div");
+    lineBreaker.setAttribute("class", "row m-1");
+    divCollector.appendChild(lineBreaker);
+    var firstButtons = document.createElement("button");
+    firstButtons.setAttribute(
+      "class",
+      "btn btn-primary btn-sm button-left-margin answer-button"
+    );
+    firstButtons.setAttribute("data-answer", questionOneAnswers[i]);
+    firstButtons.setAttribute(
+      "onclick",
+      "eventListenerWorkaroundOne.call(this)"
+    );
+    firstButtons.innerHTML = questionOneAnswers[i];
+    lineBreaker.appendChild(firstButtons);
+  }
+}
+
+function eventListenerWorkaroundOne() {
+  var answerCheck = this.getAttribute("data-answer");
+  if (answerCheck !== "3. alerts") {
+    timeLeft -= 10;
+  }
+  while (quizBody.lastChild) {
+    quizBody.removeChild(quizBody.lastChild);
+  }
+  quizTitleText.innerHTML = questions[1].question;
+  function createSecondButtons() {
+    for (i = 0; i < questionTwoAnswers.length; i++) {
+      var divCollector = document.createElement("div");
+      divCollector.setAttribute("class", "delete-me");
+      quizBody.appendChild(divCollector);
+      var lineBreaker = document.createElement("div");
+      lineBreaker.setAttribute("class", "row m-1");
+      quizBody.appendChild(lineBreaker);
+      var secondButtons = document.createElement("button");
+      secondButtons.setAttribute(
+        "class",
+        "btn btn-primary btn-sm button-left-margin answer-button"
+      );
+      secondButtons.setAttribute("data-answer", questionTwoAnswers[i]);
+      secondButtons.setAttribute(
+        "onclick",
+        "eventListenerWorkaroundTwo.call(this)"
+      );
+      secondButtons.innerHTML = questionTwoAnswers[i];
+      lineBreaker.appendChild(secondButtons);
+    }
+  }
+  createSecondButtons();
+}
+
+function eventListenerWorkaroundTwo() {
+  var answerCheck = this.getAttribute("data-answer");
+  if (answerCheck !== "3. parentheses") {
+    timeLeft -= 10;
+  }
+  while (quizBody.lastChild) {
+    quizBody.removeChild(quizBody.lastChild);
+  }
+  quizTitleText.innerHTML = questions[2].question;
+  function createThirdButtons() {
+    for (i = 0; i < questionThreeAnswers.length; i++) {
+      var divCollector = document.createElement("div");
+      divCollector.setAttribute("class", "delete-me");
+      quizBody.appendChild(divCollector);
+      var lineBreaker = document.createElement("div");
+      lineBreaker.setAttribute("class", "row m-1");
+      quizBody.appendChild(lineBreaker);
+      var thirdButtons = document.createElement("button");
+      thirdButtons.setAttribute(
+        "class",
+        "btn btn-primary btn-sm button-left-margin answer-button"
+      );
+      thirdButtons.setAttribute("data-answer", questionThreeAnswers[i]);
+      thirdButtons.setAttribute(
+        "onclick",
+        "eventListenerWorkaroundThree.call(this)"
+      );
+      thirdButtons.innerHTML = questionThreeAnswers[i];
+      lineBreaker.appendChild(thirdButtons);
+    }
+  }
+  createThirdButtons();
+}
+
+function eventListenerWorkaroundThree() {
+  var answerCheck = this.getAttribute("data-answer");
+  if (answerCheck !== "4. all of the above") {
+    timeLeft -= 10;
+  }
+  while (quizBody.lastChild) {
+    quizBody.removeChild(quizBody.lastChild);
+  }
+  quizTitleText.innerHTML = questions[3].question;
+  function createFourthButtons() {
+    for (i = 0; i < questionFourAnswers.length; i++) {
+      var divCollector = document.createElement("div");
+      divCollector.setAttribute("class", "delete-me");
+      quizBody.appendChild(divCollector);
+      var lineBreaker = document.createElement("div");
+      lineBreaker.setAttribute("class", "row m-1");
+      quizBody.appendChild(lineBreaker);
+      var fourthButtons = document.createElement("button");
+      fourthButtons.setAttribute(
+        "class",
+        "btn btn-primary btn-sm button-left-margin answer-button"
+      );
+      fourthButtons.setAttribute("data-answer", questionFourAnswers[i]);
+      fourthButtons.setAttribute(
+        "onclick",
+        "eventListenerWorkaroundFour.call(this)"
+      );
+      fourthButtons.innerHTML = questionFourAnswers[i];
+      lineBreaker.appendChild(fourthButtons);
+    }
+  }
+  createFourthButtons();
+}
+
+function eventListenerWorkaroundFour() {
+  var answerCheck = this.getAttribute("data-answer");
+  if (answerCheck !== "3. quotes") {
+    timeLeft -= 10;
+  }
+  while (quizBody.lastChild) {
+    quizBody.removeChild(quizBody.lastChild);
+  }
+  quizTitleText.innerHTML = questions[4].question;
+  function createFifthButtons() {
+    for (i = 0; i < questionFiveAnswers.length; i++) {
+      var divCollector = document.createElement("div");
+      divCollector.setAttribute("class", "delete-me");
+      quizBody.appendChild(divCollector);
+      var lineBreaker = document.createElement("div");
+      lineBreaker.setAttribute("class", "row m-1");
+      quizBody.appendChild(lineBreaker);
+      var fifthButtons = document.createElement("button");
+      fifthButtons.setAttribute(
+        "class",
+        "btn btn-primary btn-sm button-left-margin answer-button"
+      );
+      fifthButtons.setAttribute("data-answer", questionFiveAnswers[i]);
+      fifthButtons.setAttribute(
+        "onclick",
+        "eventListenerWorkaroundFive.call(this)"
+      );
+      fifthButtons.innerHTML = questionFiveAnswers[i];
+      lineBreaker.appendChild(fifthButtons);
+    }
+  }
+  createFifthButtons();
+}
+
+function eventListenerWorkaroundFive() {
+  var answerCheck = this.getAttribute("data-answer");
+  if (answerCheck !== "4. console.log") {
+    timeLeft -= 10;
+  }
+  while (quizBody.lastChild) {
+    quizBody.removeChild(quizBody.lastChild);
+  }
+  quizTitleText.innerHTML = "And that's it!";
+  var divCollector = document.createElement("div");
+  divCollector.setAttribute("class", "delete-me");
+  quizBody.appendChild(divCollector);
+}
+
+
